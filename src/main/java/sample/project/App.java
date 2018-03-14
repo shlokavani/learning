dc02ffe2604ef866eba322d994393c72bbc90df0
vani <srivani@unique-creations.biz>
2018-03-14 12:34:29 +0530

    my first commit

---
+++ src/main/java/sample/project/App.java
@@ -0,0 +1,10 @@
+package sample.project;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jenkins World!" );
+    }
+}
---
+++ src/test/java/sample/project/Test_Sample.java
@@ -0,0 +1,10 @@
+package sample.project;
+
+import org.testng.annotations.Test;
+
+public class Test_Sample {
+	@Test
+	public  void main() {
+			System.out.println("Hello Jenkins World! So excited to view the result");
+	}
+}
